9769c259cfec3f98a1dcaf7d27cd1641f33bb689
Andy Hoegh <andrew.hoegh@montana.edu>
2019-11-14 13:03:53 -0700

    added function for simulating agents

---
+++ R/sim_agents.R
@@ -0,0 +1,99 @@
+#' A Function to run a particle filter
+#'
+#' @param num_agents The number of agents to simulate
+#' @param mu_true Value for mean step size in truncated normal distribution
+#' @param sigma_true Value for the standard deviation of mean step size in truncated normal distribution
+#' @param kappa_true Value for the concentration parameter in Von Mises distribution
+#' @param sigmasq_eta Value for eta
+#' @param sigmasq_eps Value for eps
+#' @param x_range Range of x-values to simulate starting points of agents
+#' @param y_range Range of y-values to simulate starting points of agents
+#' @return An array of dimension (num_agents X time_points x 2) containing the agent locations
+#' @export
+sim_agents <- function(num_agents, time_points, mu_true, sigma_true, kappa_true,
+sigmasq_eps, sigmasq_eta, x_range, y_range){
+  z <- s <- delta <- array(0, dim=c(num_agents, time_points, 2))
+
+  u <- theta <- matrix(0, num_agents, time_points)
+  theta[,1] <- circular::rvonmises(num_agents, mu = circular::circular(0), kappa = .1)
+
+  ## starting locations
+
+  s[,1,] <- cbind(stats::runif(num_agents, min = x_range[1], max = x_range[2]),
+                  stats::runif(num_agents, min = y_range[1], max = y_range[2]))
+  z[,1,] <- s[,1,] + matrix(stats::rnorm(num_agents * 2, sd = sqrt(sigmasq_eps)), nrow = num_agents, ncol = 2)
+
+  for (t in 2:time_points){
+    # update angle
+    home_path <- tibble::tibble(x =z[,1,1]  - z[,t-1, 1], y = z[,1,2]  - z[,t-1, 2])
+    home_angle <-  useful::cart2pol(home_path$x, home_path$y)$theta
+    theta[,t] <- circular::rvonmises(num_agents, mu = circular::circular(0), kappa = kappa_true)
+    delta[,t,1] <- cos(theta[,t] + home_angle)
+    delta[,t,2] <- sin(theta[,t] + home_angle)
+
+    # update speed
+    u[,t] <- truncnorm::rtruncnorm(num_agents, a = 0, b= Inf, mean = mu_true, sd = sigma_true)
+
+    # update latent agent locations
+    s[,t,] <- s[,t-1,] + u[,t] * delta[,t,] + matrix(stats::rnorm(num_agents * 2, mean = 0 , sd = sqrt(sigmasq_eta)), nrow = num_agents, ncol = 2)
+
+    # update observed locations
+    z[,t,] <- s[,t,] + matrix(stats::rnorm(num_agents * 2, sd = sqrt(sigmasq_eps)), nrow = num_agents, ncol = 2)
+  }
+  return(z)
+}
+run_smc <- function(num_particles, data, mu_val, sigma_val, kappa_val, sigmasq_eta, sigmasq_eps){
+  time_points <- nrow(data)
+  particle_values <- array(0, dim=c(time_points, num_particles, 6))
+  w <- matrix(0, nrow = num_particles, ncol = time_points)
+
+  # Time 1
+  particle_values[1,,1:2] <- LearnBayes::rmnorm(num_particles, mean = c(data[1,1], data[1,2]), varcov = diag(2)*.1)
+  theta <- circular::rvonmises(num_particles, mu = circular::circular(0), kappa = .1)
+  particle_values[1,,3] <- sin(theta)
+  particle_values[1,,3] <- cos(theta)
+  particle_values[1,,5] <- truncnorm::rtruncnorm(num_particles, a = 0, b = Inf, mean = mu_val, sd = sigma_val)
+  descendents <- array(0, dim=c(num_particles, time_points))
+
+  #calculate weights
+  log_w <- LearnBayes::dmnorm(particle_values[1,,1:2], mean = c(data[1,1],data[1,2]), varcov = diag(2) * sigmasq_eps, log = T) -
+    LearnBayes::dmnorm(particle_values[1,,1:2], mean = c(data[1,1],data[1,2]), varcov = diag(2) * .1, log = T)
+  w[,1] <- exp(log_w)
+  log_w <- smcUtils::renormalize(log_w, log = T)
+  descendents[,1] <- sample(num_particles, replace = T, prob = log_w)
+  particle_values[1,,] <- particle_values[1,descendents[,1] ,]
+
+  # Time 2:T
+  for (t in 2:time_points){
+    # propose angles
+    home_path <- tibble::tibble(x = data[1, 1] - particle_values[t-1,,1]  ,
+                                y = data[1, 2] - particle_values[t-1,,2]  )
+    home_angle <-  useful::cart2pol(home_path$x, home_path$y)$theta
+    particle_values[t,,6] <- circular::rvonmises(num_particles, mu = circular::circular(0), kappa = kappa_val)
+    particle_values[t,,3] <- cos(particle_values[t,,6] + home_angle) # x coord
+    particle_values[t,,4] <- sin(particle_values[t,,6] + home_angle) # y coord
+
+    # propose distance
+    particle_values[t,,5] <- truncnorm::rtruncnorm(num_particles, a = 0, b= Inf, mean = mu_val, sd = sigma_val)
+
+    # update particle locations
+    particle_values[t,,1:2] <- particle_values[t-1,,1:2] + particle_values[t,,5] * particle_values[t,,3:4] +
+      stats::rnorm(num_particles * 2, mean = 0, sd = sqrt(sigmasq_eta))
+
+    # calculate weights
+    log_w <- LearnBayes::dmnorm(particle_values[t,,1:2], mean = c(data[t,1], data[t,2]), varcov = diag(2) * sigmasq_eps, log = T)
+    w[,t] <- exp(log_w)
+    log_w <- smcUtils::renormalize(log_w, log = T)
+    descendents[,t] <- sample(num_particles, replace = T, prob = log_w)
+    particle_values[t,,] <- particle_values[t, descendents[,t],]
+  }
+  index <- rep(0, time_points)
+  index[time_points] <- sample(num_particles, 1)
+  path <- array(0, dim=c(time_points,6))
+  path[time_points,] <- particle_values[time_points,index[time_points],]
+  for (iter in time_points:2){
+    index[iter- 1] <- descendents[index[iter], iter]
+    path[iter - 1,] <- particle_values[iter - 1, index[iter-1],]
+  }
+  return(list(path = path, log_pi = sum(log(colMeans(w)))))
+}
